9f97131124a0f76bc8135c166ad9f284234db086
Vincent Descamps <vincedescamps@gmail.com>
2021-06-30 10:49:32 +0200

    ajout flip cards

--- src/pages/Jesus.vue
+++ src/pages/Jesus.vue
@@ -1,12 +1,197 @@
 <template>
-  <div class="jesus">
-    <h1>Jésus</h1>
+  <div class="app">
+    <header class="jesus">
+      <h1>Jésus</h1>
+      <p>
+        <cite>
+          «C’est moi qui suis le chemin, la vérité et la vie. On ne vient au
+          Père qu’en passant par moi.»
+        </cite>
+        Jean 14:6
+      </p>
+    </header>
+    <main>
+      <h1>Pourquoi Jésus ?</h1>
+      <div class="container">
+        <div class="flip-card">
+          <div class="card">
+            <div class="card-front">
+              <h2>Dieu existe.</h2>
+              <img
+                src="@/assets/hands.jpg"
+                alt="photo of hands reaching for each other"
+              />
+            </div>
+            <div class="card-back">
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Explicabo, dolores provident labore molestiae ex perspiciatis
+                porro quam, illo ab in quas neque magnam maiores! Magni recusandae
+                quod dolore temporibus optio!
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="flip-card">
+          <div class="card">
+            <div class="card-front">
+              <h2>Dieu existe.</h2>
+              <img
+                src="@/assets/hands.jpg"
+                alt="photo of hands reaching for each other"
+              />
+            </div>
+            <div class="card-back">
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Explicabo, dolores provident labore molestiae ex perspiciatis
+                porro quam, illo ab in quas neque magnam maiores! Magni recusandae
+                quod dolore temporibus optio!
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="flip-card">
+          <div class="card">
+            <div class="card-front">
+              <h2>Dieu existe.</h2>
+              <img
+                src="@/assets/hands.jpg"
+                alt="photo of hands reaching for each other"
+              />
+            </div>
+            <div class="card-back">
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Explicabo, dolores provident labore molestiae ex perspiciatis
+                porro quam, illo ab in quas neque magnam maiores! Magni recusandae
+                quod dolore temporibus optio!
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="flip-card">
+          <div class="card">
+            <div class="card-front">
+              <h2>Dieu existe.</h2>
+              <img
+                src="@/assets/hands.jpg"
+                alt="photo of hands reaching for each other"
+              />
+            </div>
+            <div class="card-back">
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Explicabo, dolores provident labore molestiae ex perspiciatis
+                porro quam, illo ab in quas neque magnam maiores! Magni recusandae
+                quod dolore temporibus optio!
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="flip-card">
+          <div class="card">
+            <div class="card-front">
+              <h2>Dieu existe.</h2>
+              <img
+                src="@/assets/hands.jpg"
+                alt="photo of hands reaching for each other"
+              />
+            </div>
+            <div class="card-back">
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Explicabo, dolores provident labore molestiae ex perspiciatis
+                porro quam, illo ab in quas neque magnam maiores! Magni recusandae
+                quod dolore temporibus optio!
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="flip-card">
+          <div class="card">
+            <div class="card-front">
+              <h2>Dieu existe.</h2>
+              <img
+                src="@/assets/hands.jpg"
+                alt="photo of hands reaching for each other"
+              />
+            </div>
+            <div class="card-back">
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Explicabo, dolores provident labore molestiae ex perspiciatis
+                porro quam, illo ab in quas neque magnam maiores! Magni recusandae
+                quod dolore temporibus optio!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-export default {
-  
-}
- 
+export default {};
 </script>
+
+<style scoped>
+main h1 {
+  text-align: center;
+  padding: 80px;
+}
+.container{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+.flip-card {
+  margin: 50px;
+  background-color: transparent;
+  width: 400px;
+  height: 300px;
+  perspective: 1000px; /* Remove this if you don't want the 3D effect */
+}
+
+/* This container is needed to position the front and back side */
+.card {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.9s;
+  transform-style: preserve-3d;
+}
+
+/* Do an horizontal flip when you move the mouse over the flip box container */
+.flip-card:hover .card {
+  transform: rotateY(180deg);
+}
+
+/* Position the front and back side */
+.card-front,
+.card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden; /* Safari */
+  backface-visibility: hidden;
+}
+
+/* Style the front side (fallback if image is missing) */
+.card-front {
+  /* background-color: #bbb; */
+  color: #2F819C;
+}
+
+/* Style the back side */
+.card-back {
+  background-color: dodgerblue;
+  color: white;
+  transform: rotateY(180deg);
+}
+img{
+  max-width: 400px;
+}
+</style>
--- src/pages/Unity.vue
+++ src/pages/Unity.vue
@@ -1,53 +1,52 @@
 <template>
   <div class="unity">
     <header>
-    <h1>Unité</h1>
-    <p><cite>« Maintenant donc il y a plusieurs membres et un seul corps »</cite> 1 Corinthiens 12:20</p>
+      <h1>Unité</h1>
+      <p>
+        <cite>
+          «Maintenant donc il y a plusieurs membres et un seul corps»
+        </cite>
+        1 Corinthiens 12:20
+      </p>
     </header>
     <main>
-       <h1>Évenements Chrétiens</h1>
-        <!--Publication des evenements chrétiens de la région-->
-            <event-component :events="events" />
-        <h1>Trouver un église</h1>
-            <map-component />
+      <h1>Évenements Chrétiens</h1>
+      <!--Publication des evenements chrétiens de la région-->
+      <event-component :events="events" />
+      <h1>Trouver un église</h1>
+      <map-component />
     </main>
   </div>
 </template>
 
 <script>
-import data from "@/data.json"
+import data from "@/data.json";
 //import axios from 'axios';
 import EventComponent from "@/components/EventComponent.vue";
-import MapComponent from '../components/MapComponent.vue';
+import MapComponent from "../components/MapComponent.vue";
 
 export default {
   components: {
     EventComponent,
     MapComponent,
   },
-  data () {
-        return {
-            events: data.event
-        }
-    },
-    //a remplacer par api bdd 
-    // mounted () {
-    //     axios
-    //     .get("https://newsapi.org/v2/top-headlines?country=fr&apiKey=e46b0aaf0ca445f1852d261b861e4eb0")
-    //     .then(response => (this.data = response.data.articles));
-    // }
-}
+  data() {
+    return {
+      events: data.event,
+    };
+  },
+  //a remplacer par api bdd
+  // mounted () {
+  //     axios
+  //     .get("https://newsapi.org/v2/top-headlines?country=fr&apiKey=e46b0aaf0ca445f1852d261b861e4eb0")
+  //     .then(response => (this.data = response.data.articles));
+  // }
+};
 </script>
 
 <style scoped>
-header{
-  padding: 25px;
-}
-main h1{
+main h1 {
   text-align: center;
   padding: 80px;
-}
-main{
-  padding: 60px;
 }
 </style>
