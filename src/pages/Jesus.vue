048ff4a7315fb7c4094e1ed933345f4c3d20803d
Vincent Descamps <vincedescamps@gmail.com>
2021-07-02 10:04:42 +0200

    Content des flip cards ajouté

--- src/pages/Jesus.vue
+++ src/pages/Jesus.vue
@@ -12,22 +12,25 @@
     </header>
     <main>
       <h1>Pourquoi Jésus ?</h1>
+      <h2>Qui est Jésus ?</h2>
       <div class="container">
         <div class="flip-card">
           <div class="card">
             <div class="card-front">
-              <h2>Dieu existe.</h2>
+              <h3>LA VOIE</h3>
               <img
-                src="@/assets/hands.jpg"
+                src="@/assets/path.jpeg"
                 alt="photo of hands reaching for each other"
               />
             </div>
             <div class="card-back">
+              <h4>Le Chemin</h4>
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Explicabo, dolores provident labore molestiae ex perspiciatis
-                porro quam, illo ab in quas neque magnam maiores! Magni recusandae
-                quod dolore temporibus optio!
+                <cite
+                  >Jésus lui dit: «C’est moi qui suis le chemin, la vérité et la
+                  vie. On ne vient au Père qu’en passant par moi.</cite
+                >
+                <br />Jean 14:6
               </p>
             </div>
           </div>
@@ -35,18 +38,43 @@
         <div class="flip-card">
           <div class="card">
             <div class="card-front">
-              <h2>Dieu existe.</h2>
+              <h3>LE CRÉATEUR</h3>
               <img
-                src="@/assets/hands.jpg"
+                src="@/assets/creator.jpg"
                 alt="photo of hands reaching for each other"
               />
             </div>
             <div class="card-back">
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Explicabo, dolores provident labore molestiae ex perspiciatis
-                porro quam, illo ab in quas neque magnam maiores! Magni recusandae
-                quod dolore temporibus optio!
+              <h4>Le Créateur</h4>
+              <p style="margin-top: 65px">
+                Dieu est notre créateur! Il nous a créée avec une
+                caractéristique particulière: "être à son image". Il a un plan
+                pour nos vies. Mais le péché nous sépare de Dieu et tous les
+                bons plans qu'il a préparé pour nous.
+              <br />
+                <cite>
+                  Dieu créa l'homme à son image, il le créa à l'image de Dieu.
+                  Il créa l'homme et la femme.
+                </cite>
+                <br />
+                <b>Genèse 1:27</b>
+              <br />
+              <br />
+                <cite>
+                  C'est pourquoi, de même que par un seul homme le péché est
+                  entré dans le monde, et par le péché la mort, de même la mort
+                  a atteint tous les hommes parce que tous ont péché.
+                </cite>
+                <br />
+                <b>Romain 5:12</b>
+              <br />
+                <cite>
+                  Et vous qui étiez autrefois étrangers et ennemis de Dieu par
+                  vos pensées et par vos œuvres mauvaises, il vous a maintenant
+                  réconciliés
+                </cite>
+                <br />
+                <b>Colossiens 1:21</b>
               </p>
             </div>
           </div>
@@ -54,18 +82,64 @@
         <div class="flip-card">
           <div class="card">
             <div class="card-front">
-              <h2>Dieu existe.</h2>
+              <h3>LE FILS</h3>
               <img
-                src="@/assets/hands.jpg"
+                src="@/assets/sonOfGod.jpeg"
                 alt="photo of hands reaching for each other"
               />
             </div>
             <div class="card-back">
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Explicabo, dolores provident labore molestiae ex perspiciatis
-                porro quam, illo ab in quas neque magnam maiores! Magni recusandae
-                quod dolore temporibus optio!
+              <h4>Le Fils</h4>
+              <p style="margin-top: 30px">
+                La Bible nous dit que la réponse de Dieu au péché fut d'envoyer
+                son Fils unique sur terre afin que nous connaissions la nature
+                et le coeur de Dieu.
+                <br />
+                <cite>
+                  En effet, Dieu a tant aimé le monde qu'il a donné son Fils
+                  unique afin que quiconque croit en lui ne périsse pas mais ait
+                  la vie éternelle.
+                </cite>
+                <br />
+                <b>Jean 3:16</b>
+                <br />
+                <br />
+                <cite>
+                  En effet, la loi a été donnée à travers Moïse, mais la grâce
+                  et la vérité sont venues à travers Jésus-Christ.
+                </cite>
+                <br />
+                <b>Jean 1:17</b>
+              </p>
+              <br />
+              <br />
+            </div>
+          </div>
+        </div>
+        <div class="flip-card">
+          <div class="card">
+            <div class="card-front">
+              <h3>LA MORT ET LA RÉSURRECTION</h3>
+              <img
+                src="@/assets/resurrection.jpeg"
+                alt="photo of hands reaching for each other"
+              />
+            </div>
+            <div class="card-back">
+              <h4>La mort et la résurrection</h4>
+              <p style="margin-top: 60px">
+                Les amis et les ennemis de Jésus étaient d’accord pour dire que
+                son tombeau était vide. Comment a-t-il pu le devenir? Les
+                évangélistes disent que Jésus est ressuscité d’entre les morts.
+                Les autres explications sont peu convaincantes. En outre, après
+                la résurrection, beaucoup de gens ont vu Jésus vivant: «Ensuite
+                il [Jésus] a été vu de plus de cinq cents frères à la fois, dont
+                la plupart sont demeurés en vie jusqu’à présent, mais certains
+                aussi se sont endormis [ou: sont morts]. Ensuite il a été vu de
+                Jacques, puis de tous les apôtres» (1Cor. 15:6-7). La
+                résurrection physique de Jésus-Christ est la meilleure
+                explication des faits, et elle me permet de croire que Jésus
+                était la véritable révélation de Dieu.
               </p>
             </div>
           </div>
@@ -73,18 +147,26 @@
         <div class="flip-card">
           <div class="card">
             <div class="card-front">
-              <h2>Dieu existe.</h2>
+              <h3>LA RÉCONCILIATION</h3>
               <img
                 src="@/assets/hands.jpg"
                 alt="photo of hands reaching for each other"
               />
             </div>
             <div class="card-back">
+              <h4>Réconciliation</h4>
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Explicabo, dolores provident labore molestiae ex perspiciatis
-                porro quam, illo ab in quas neque magnam maiores! Magni recusandae
-                quod dolore temporibus optio!
+                La mort du Christ, dans son corps physique, nous permet de
+                retrouver une relation avec Dieu.
+                <br />
+                <br />
+                <cite>
+                  il vous a maintenant réconciliés par la mort [de son Fils]
+                  dans son corps de chair pour vous faire paraître devant lui
+                  saints, sans défaut et sans reproche.
+                </cite>
+                <br />
+                <b>Colossiens 1:22</b>
               </p>
             </div>
           </div>
@@ -92,37 +174,24 @@
         <div class="flip-card">
           <div class="card">
             <div class="card-front">
-              <h2>Dieu existe.</h2>
+              <h3>NOUVELLE VIE</h3>
               <img
-                src="@/assets/hands.jpg"
+                src="@/assets/chains.jpeg"
                 alt="photo of hands reaching for each other"
               />
             </div>
             <div class="card-back">
+              <h4>Nouvelle Vie</h4>
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Explicabo, dolores provident labore molestiae ex perspiciatis
-                porro quam, illo ab in quas neque magnam maiores! Magni recusandae
-                quod dolore temporibus optio!
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="flip-card">
-          <div class="card">
-            <div class="card-front">
-              <h2>Dieu existe.</h2>
-              <img
-                src="@/assets/hands.jpg"
-                alt="photo of hands reaching for each other"
-              />
-            </div>
-            <div class="card-back">
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Explicabo, dolores provident labore molestiae ex perspiciatis
-                porro quam, illo ab in quas neque magnam maiores! Magni recusandae
-                quod dolore temporibus optio!
+                La foi chrétienne est une foi rationnelle. Elle n’est pas une
+                marche aveugle dans l’obscurité. Elle est sensée. Elle est digne
+                de confiance. De plus, le christianisme authentique fonctionne
+                et il satisfait le besoin fondamental de l’être humain: il
+                m‘apporte l’espoir face à ce monde brisé, il donne un sens et
+                une direction à ma vie, et je suis convaincu qu’il fait de moi
+                un homme meilleur. Lorsque Jésus a affirmé «moi, je suis venu
+                afin qu’elles [les brebis] aient la vie, et qu’elles l’aient en
+                abondance» (Jean 10:10), il disait la vérité.
               </p>
             </div>
           </div>
@@ -141,7 +210,7 @@ main h1 {
   text-align: center;
   padding: 80px;
 }
-.container{
+.container {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
@@ -151,7 +220,7 @@ main h1 {
   background-color: transparent;
   width: 400px;
   height: 300px;
-  perspective: 1000px; /* Remove this if you don't want the 3D effect */
+  /*perspective: 1000px; /* Remove this if you don't want the 3D effect */
 }
 
 /* This container is needed to position the front and back side */
@@ -164,34 +233,51 @@ main h1 {
   transform-style: preserve-3d;
 }
 
-/* Do an horizontal flip when you move the mouse over the flip box container */
 .flip-card:hover .card {
   transform: rotateY(180deg);
 }
 
-/* Position the front and back side */
 .card-front,
 .card-back {
   position: absolute;
   width: 100%;
   height: 100%;
-  -webkit-backface-visibility: hidden; /* Safari */
+  -webkit-backface-visibility: hidden;
   backface-visibility: hidden;
 }
 
-/* Style the front side (fallback if image is missing) */
 .card-front {
-  /* background-color: #bbb; */
-  color: #2F819C;
+  color: #2f819c;
 }
 
 /* Style the back side */
 .card-back {
-  background-color: dodgerblue;
+  background-color: #2f819c;
   color: white;
   transform: rotateY(180deg);
+  overflow: scroll;
 }
-img{
+img {
   max-width: 400px;
+}
+main p {
+  margin: auto;
+  width: 20em;
+  position: relative;
+  top: 50%;
+  transform: perspective(1px) translateY(-50%);
+}
+h3 {
+  margin-top: 10px;
+}
+.card-back h4 {
+  padding: 10px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  width: 100px;
+  border-bottom: 2px solid #cf2418;
 }
 </style>
